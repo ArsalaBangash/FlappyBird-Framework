796e90b4c8e918b83d7a2fbbc83f579b2abc3c41
Vaishvik <vaishvik.maisuria@gmail.com>
2018-05-22 10:59:32 -0400

    proper commenting

--- flappy.py
+++ flappy.py
@@ -1,11 +1,21 @@
 
 
-'This is a integer varible you can  change its value to make the game go faster or slower
+'Varibles
+
+
+' This is a string variable
+## this variable contains the name of the game which shoes up on the title bar. 
+name_of_game = 'Flappy Bird'
+
+'This is a Integer Varible
+## This variable controls speed of the game - you can  change its value to make the game go faster or slower
 game_speed = 30     
 
-'This is a variable that stores a tuple of 3 pathway to images that represent the three possible position of the bird flap'
+'This is a Boolean Variable (229) - Idk how to replace this variable needd to ask...
+## This is a variable that stores if the player flaps
+player_flap = False
+
+'This is a variable that stores a tuple of 3 Strings
+## This is variable stores a pathway to images that represent the three possible position of the bird flap'
 red_bird =  ('assets/sprites/redbird-upflap.png','assets/sprites/redbird-midflap.png','assets/sprites/redbird-downflap.png',)
 
-
-' This is a string variable which contains the name of the game which shoes up on the title bar. 
-name_of_game = 'Flappy Bird'
